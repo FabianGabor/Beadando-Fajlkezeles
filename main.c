3977e7fe391a18dac4dfaacdeb3ca60d046ce612
Fábián Gábor <info@fabiangabor.com>
2019-11-29 21:19:25 +0100

    init

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main()
-{
+{    
+    // Készíts programot, ami egy létező szöveges fájl tartalmát úgy írja ki a képernyőre, hogy a sorok elé odaírja a sorszámot is!
+
+    // Készíts programot, ami a felhasználó nevét és életkorát kiírja egy fájlba!
+
+    // Ha már létezik a 2. program által létrehozott fájl, olvastassuk vissza egy új programban a korábbi eredményeket!
+
+    // Kérj be 0 végjelig valós számokat, majd írasd ki őket egy fájlba!
+
 
     return 0;
 }
